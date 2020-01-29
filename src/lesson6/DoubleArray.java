202dbd7f4853439d251561ef4e193339c5f6a87c
Anna Goncharova <anna.s.goncharova.2001@gmail.com>
2020-01-29 16:26:23 +0200

    DoubleArray inited

---
+++ src/lesson6/DoubleArray.java
@@ -0,0 +1,15 @@
+/*
+Создать массив типа double с размером 4.
+Записать в него любые значения с помощью блока для инициализации.
+ Вывести на консоль значение первого элемента.
+ */
+package lesson6;
+
+public class DoubleArray {
+    public static void main( String[] args ) {
+
+                int[] weekDays = { 1, 2, 3, 4, 5, 6, 7};
+                System.out.println("Понедельник " + weekDays[0] + "й день в недели.");
+            }
+}
+
