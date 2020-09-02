2679a9b603dc205e829935d9e3e98bab85e588c8
Anna Goncharova <anna.s.goncharova.2001@gmail.com>
2020-09-02 21:20:06 +0300

    threads

---
+++ src/threads/MyFirstThread.java
@@ -0,0 +1,9 @@
+package threads;
+
+public class MyFirstThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("I'm Thread! My name is " + getName());
+    }
+}
